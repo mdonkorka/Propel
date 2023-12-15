caae3a0297dd7014a49c36ace3768105e718dc7a
mdonkorka <74377231+mdonkorka@users.noreply.github.com>
2023-12-15 20:49:20 +0000

    React set up

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Info from './Info.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Info/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Info.jsx
@@ -0,0 +1,12 @@
+
+
+function Info() {
+
+  return (
+    <p>
+      Please enter your sign in information
+    </p>
+  )
+} 
+
+export default Info
